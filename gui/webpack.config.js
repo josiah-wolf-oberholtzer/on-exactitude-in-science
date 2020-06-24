335bab5141ff3366fb9995a21297d53ea5f7ccf5
Josiah Oberholtzer <josiah.oberholtzer@gmail.com>
2020-06-23 22:30:40 -0400

    More JS tinkering

--- gui/webpack.config.js
+++ gui/webpack.config.js
@@ -1,9 +1,28 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    })
+  ]
 };
