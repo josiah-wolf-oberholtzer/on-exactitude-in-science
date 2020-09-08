dc50dcfb11585eb0e66a5662aa4a964c19fc7f48
Josiah Oberholtzer <josiah.oberholtzer@gmail.com>
2020-09-07 20:25:31 -0400

    Implementing pagination

---
+++ gui/src/components/SidebarPagination.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import * as QueryString from 'query-string';
+import { Box, Divider, ListItem } from '@material-ui/core';
+import Pagination from '@material-ui/lab/Pagination';
+import { connect } from 'react-redux';
+import { push } from 'connected-react-router';
+import { queryObjectToString, queryStringToObject } from '../utils';
+import { useLocation } from "react-router-dom";
+
+const mapStateToProps = state => {
+  return { }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    pushPage: (location, page) => {
+      const parsedQuery = queryStringToObject(location.search);
+      if (page === 1) {
+        delete parsedQuery.page;
+      } else {
+        parsedQuery.page = page;
+      }
+      dispatch(push(location.pathname + "?" + queryObjectToString(parsedQuery)));
+    }
+  }
+}
+
+const SidebarPagination = (props) => {
+  const location = useLocation();
+  return (
+    <React.Fragment>
+      <ListItem>
+        <Box 
+          display="flex" 
+          width="100%"
+          alignItems="center"
+          justifyContent="center"
+          pb={1}
+        >
+          <Pagination
+            count={10}
+            onChange={(event, value) => { props.pushPage(location, value) }}
+          />
+        </Box>
+      </ListItem>
+      <Divider />
+    </React.Fragment>
+  )
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SidebarPagination);
