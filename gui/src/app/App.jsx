5f10d3213250fced4987269ab8d9df5e8215b375
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2020-09-19 14:43:24 -0400

    Improve error messaging (#32)

--- gui/src/app/App.jsx
+++ gui/src/app/App.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
-import { CssBaseline, ThemeProvider, makeStyles, } from '@material-ui/core';
+import { Button, CssBaseline, ThemeProvider, makeStyles, } from '@material-ui/core';
 import { ConnectedRouter } from 'connected-react-router'
 import { history } from './store';
 import { theme } from './theme';
 import EntityCaption from '../components/EntityCaption';
 import EntityDial from '../components/EntityDial';
-import ErrorMessage from '../components/ErrorMessage';
 import Graph from '../components/Graph';
 import Header from '../components/Header';
 import Loading from '../components/Loading';
 import Routing from '../components/Routing';
 import Sidebar from '../components/Sidebar';
+import ErrorMessage from '../components/ErrorMessage';
+import { SnackbarProvider } from 'notistack';
 
 const useStyles = makeStyles((theme) => ({
   root: { display: 'flex' },
 }));
+
+const notistackRef = React.createRef();
+
+const onClickDismiss = key => () => { notistackRef.current.closeSnackbar(key); }
 
 const App = () => {
   const classes = useStyles();
@@ -22,16 +27,26 @@ const App = () => {
     <ConnectedRouter history={history} noInitialPop>
       <Routing />
       <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className={classes.root}>
-          <Graph />
-          <EntityDial />
-          <EntityCaption />
-          <Header />
-          <Sidebar />
-          <ErrorMessage />
-          <Loading />
-        </div>
+        <SnackbarProvider
+          action={(key) => ( 
+            <Button onClick={onClickDismiss(key)}>
+              Dismiss
+            </Button>
+          )}
+          maxSnack={3}
+          ref={notistackRef}
+        >
+          <CssBaseline />
+          <div className={classes.root}>
+            <Graph />
+            <EntityDial />
+            <EntityCaption />
+            <Header />
+            <Sidebar />
+            <Loading />
+            <ErrorMessage />
+          </div>
+        </SnackbarProvider>
       </ThemeProvider>
     </ConnectedRouter>
   )
