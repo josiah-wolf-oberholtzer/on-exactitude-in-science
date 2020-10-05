f40707c7edbd0051529c6c983b45f36ba8d47248
Josiah Oberholtzer <josiah.oberholtzer@gmail.com>
2020-10-05 09:42:07 -0400

    upsert_vertex returns map

--- api/maps/loader.py
+++ api/maps/loader.py
@@ -9,11 +9,15 @@ from pathlib import Path
 
 from aiogremlin.exception import GremlinServerError
 from aiogremlin.process.graph_traversal import __
-from gremlin_python.process.traversal import Cardinality, P
+from gremlin_python.process.traversal import Cardinality, P, WithOptions
 
 from maps import entities, goblin, xml
 
 logger = logging.getLogger(__name__)
+
+
+async def backoff(attempts, timeout=1.0, backoff_factor=1.5):
+    await asyncio.sleep(timeout * pow(backoff_factor, attempts))
 
 
 async def drop():
@@ -39,6 +43,7 @@ async def load(goblin_app, path, consumer_count=1, limit=None):
     ]
     await asyncio.gather(*tasks)
     logger.info("Loaded data in {}".format(datetime.datetime.now() - start_date))
+    # TODO: Purge vertices untouched during loading.
 
 
 def producer(path: Path, consumer_count=1, limit=None):
@@ -54,6 +59,66 @@ def producer(path: Path, consumer_count=1, limit=None):
             yield i, entity
     for _ in range(consumer_count):
         yield None
+
+
+async def upsert_vertex(xml_entity, session):
+    kind = type(xml_entity).__name__
+    goblin_entity = getattr(entities, kind)()
+    label = kind.lower()
+    entity_key = f"{label}_id"
+    traversal = (
+        session.g.V()
+        .has(label, entity_key, xml_entity.entity_id)
+        .fold()
+        .coalesce(
+            __.unfold(),
+            __.addV(label).property(entity_key, xml_entity.entity_id),
+        )
+        .property("last_modified", time.time())
+        .property("random", random.random())
+    )
+    for key, value in dataclasses.asdict(xml_entity).items():
+        if value is None or key == entity_key or not hasattr(goblin_entity, key):
+            continue
+        if isinstance(value, (set, list)):
+            for subvalue in value:
+                traversal = traversal.property(Cardinality.set_, key, subvalue)
+        else:
+            traversal = traversal.property(Cardinality.single, key, value)
+    traversal = traversal.valueMap().with_(WithOptions.tokens)
+    for attempt in range(10):
+        try:
+            return await traversal.next()
+        except GremlinServerError as e:
+            logger.error(f"Backing off: {e!s}\n{traceback.format_exc()}")
+            await backoff(attempt)
+    # TODO: Return entity map, so we can purge stale properties
+
+
+async def upsert_edge(
+    session, *, name, source, target, primacy=0, source_label=0, target_label=0
+):
+    from_label, from_id = source
+    to_label, to_id = target
+    traversal = (
+        session.g.V()
+        .has(from_label, f"{from_label}_id", from_id)
+        .addE("relationship")
+        .to(__.V().has(to_label, f"{to_label}_id", to_id))
+        .property("last_modified", time.time())
+        .property("name", name)
+        .property("primacy", primacy)
+        .property("source_label", source_label)
+        .property("target_label", target_label)
+    )
+    for attempt in range(10):
+        try:
+            await traversal.fold().toList()
+        except GremlinServerError as e:
+            if "The provided traverser does not map to a value" in str(e):
+                return
+            logger.error(f"Backing off: {e!s}\n{traceback.format_exc()}")
+            await backoff(attempt)
 
 
 async def vertex_consumer(goblin_app, iterator, consumer_id=1, timestamp=0.0):
@@ -223,66 +288,3 @@ async def load_release_edges(xml_release, session):
             primacy=primacy,
             name="Subrelease Of",
         )
-
-
-async def upsert_vertex(xml_entity, session):
-    kind = type(xml_entity).__name__
-    goblin_entity = getattr(entities, kind)()
-    label = kind.lower()
-    entity_key = f"{label}_id"
-    traversal = (
-        session.g.V()
-        .has(label, entity_key, xml_entity.entity_id)
-        .fold()
-        .coalesce(
-            __.unfold(),
-            __.addV(label).property(entity_key, xml_entity.entity_id),
-        )
-        .property("last_modified", time.time())
-        .property("random", random.random())
-    )
-    for key, value in dataclasses.asdict(xml_entity).items():
-        if value is None or key == entity_key or not hasattr(goblin_entity, key):
-            continue
-        if isinstance(value, (set, list)):
-            for subvalue in value:
-                traversal = traversal.property(Cardinality.set_, key, subvalue)
-        else:
-            traversal = traversal.property(Cardinality.single, key, value)
-    traversal = traversal.id()
-    for attempt in range(10):
-        try:
-            return await traversal.next()
-        except GremlinServerError as e:
-            logger.error(f"Backing off: {e!s}\n{traceback.format_exc()}")
-            await backoff(attempt)
-
-
-async def upsert_edge(
-    session, *, name, source, target, primacy=0, source_label=0, target_label=0
-):
-    from_label, from_id = source
-    to_label, to_id = target
-    traversal = (
-        session.g.V()
-        .has(from_label, f"{from_label}_id", from_id)
-        .addE("relationship")
-        .to(__.V().has(to_label, f"{to_label}_id", to_id))
-        .property("last_modified", time.time())
-        .property("name", name)
-        .property("primacy", primacy)
-        .property("source_label", source_label)
-        .property("target_label", target_label)
-    )
-    for attempt in range(10):
-        try:
-            await traversal.fold().toList()
-        except GremlinServerError as e:
-            if "The provided traverser does not map to a value" in str(e):
-                return
-            logger.error(f"Backing off: {e!s}\n{traceback.format_exc()}")
-            await backoff(attempt)
-
-
-async def backoff(attempts, timeout=1.0, backoff_factor=1.5):
-    await asyncio.sleep(timeout * pow(backoff_factor, attempts))
