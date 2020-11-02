4b909392ecc0868921272fb85c37974d09355415
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2020-11-02 12:18:47 -0500

    Use LXML (#35)
    * Use LXML
    
    * Reformat

--- api/maps/k8s.py
+++ api/maps/k8s.py
@@ -3,10 +3,9 @@ from tqdm.utils import _unicode
 
 
 class TQDMK8S(tqdm):
-
     @staticmethod
     def status_printer(fp):
-        fp_flush = getattr(fp, 'flush', lambda: None)  # pragma: no cover
+        fp_flush = getattr(fp, "flush", lambda: None)  # pragma: no cover
 
         def fp_write(s):
             fp.write(_unicode(s))
